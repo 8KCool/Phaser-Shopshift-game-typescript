b2d2117f3e54481d281d7398f677c554986d65fc
BlackPandalancer <8klancer@gmail.com>
2023-04-28 02:44:38 +0000

    feat: add progress bar

--- src/scenes/loading.ts
+++ src/scenes/loading.ts
@@ -12,6 +12,55 @@ export class LoadingScene extends Scene {
         this.loadAssets();
     }
 
+    private initProgressBar() {
+        const halfWidth = getGameWidth(this) * 0.5;
+        const posY = getGameHeight(this) - 50;
+
+        const progressBarHeight = 30;
+        const progressBarWidth = halfWidth * 2 - 100;
+
+        const progressBar = this.add.rectangle(
+            halfWidth + 20 - progressBarWidth * 0.5,
+            posY,
+            10,
+            progressBarHeight - 20,
+            0xFFFFFF,
+        );
+
+        const loadingText = this.add.text(halfWidth - 75, posY - 100, 'Loading...').setFontSize(24);
+        const percentText = this.add.text(halfWidth - 25, posY, '0%').setFontSize(24);
+        const assetText = this.add.text(halfWidth - 25, posY + 100, '').setFontSize(24);
+
+        var percent = 0;
+        this.load.on('progress', (value: any) => {
+            progressBar.width = (progressBarWidth - 30) * value * 0.2;  // temp progress real limit value
+
+            percent = value * 100 * 0.2;
+            percentText.setText(`${percent}%`);
+        });
+
+        this.load.on('fileprogress', (file: any) => {
+            assetText.setText(file.key);
+        });
+
+        this.load.on('complete', () => {
+            var intId = setInterval(function () {
+                if(percent == 100) {
+                    loadingText.destroy();
+                    percentText.destroy();
+                    assetText.destroy();
+                    progressBar.destroy();
+                    clearInterval(intId);
+                }
+                else {
+                    progressBar.width = (progressBarWidth - 30) * percent / 100;
+                    percent ++;
+                    percentText.setText(`${percent}%`);
+                }
+            }, 100);  // temp progress speed
+        })
+    }
+
     private loadAssets() {
         this.load.baseURL = 'assets/';
         this.load.image('bg-wall', 'sprites/bg-wall.png');
